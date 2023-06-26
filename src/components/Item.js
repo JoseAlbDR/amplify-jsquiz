4fc73ece0083688dec8a1e9b7bfc9ca7c5ab1b6b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-26 17:10:05 +0200

    Add Item component for Accordion component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,13 @@
 export default function Item({ num, title, children, onOpen, curOpen }) {
+  console.log(curOpen);
   const isOpen = num === curOpen;
+  console.log(isOpen);
 
   return (
     <div onClick={() => onOpen(num)} className={`item ${isOpen ? "open" : ""}`}>
-      <p className="btn title">{title}</p>
+      <p className="number"></p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
       {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
