bce3c5a06799ada6c1224f242bae6a91284133b6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-28 12:01:24 +0200

    feat: add percentages to correct and wrong answers stadistics

--- src/components/Stadistics.js
+++ src/components/Stadistics.js
@@ -1,12 +1,26 @@
 function Stadistics({ userData }) {
   // console.log(userData);
+  const correctPercentage = Math.round(
+    (userData.correct / userData.total) * 100,
+    2
+  );
+  const incorrectPercentage = Math.round(
+    (userData.wrong / userData.total) * 100,
+    2
+  );
+
   return (
     <div>
       <p className="stadistics">
         Total questions anwered <strong>{userData.total}</strong>, total correct{" "}
-        <strong>{userData.correct}</strong>, total incorrect{" "}
-        <strong>{userData.wrong}</strong>, MaxScore{" "}
-        <strong>{userData.maxScore}</strong>
+        <strong>
+          {userData.correct}({correctPercentage}%)
+        </strong>
+        , total incorrect{" "}
+        <strong>
+          {userData.wrong}({incorrectPercentage}%)
+        </strong>
+        , HighScore <strong>{userData.maxScore}</strong>
       </p>
     </div>
   );
